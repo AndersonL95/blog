8ee92d9bf9080712b273bbf6639229457d32d692
AndersonL95 <anderson-alves@hotmail.com>
2021-01-31 18:42:23 -0300

    Criação do login BackEnd

--- controllers/usuario_controller.js
+++ controllers/usuario_controller.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 const User = require("../models/User")
 require('dotenv').config();
+const createToken = (user) => {
+    return jwt.sign({user},process.env.SECRET,{
+        expiresIn: '7d',
+    });
+};
 
 module.exports.validacaoRegistro = [
     body("nome").not().isEmpty().trim().withMessage("Preencha o campo!"),
@@ -28,9 +33,7 @@ module.exports.registro = async (req, res) =>{
                     email,
                     senha: hash,
                 });
-                const token = jwt.sign({user},process.env.SECRET,{
-                    expiresIn: '7d',
-                });
+                const token = createToken(user);
                 return res.status(200).json({msg: 'Sua conta foi criada!', token});
             } catch(error) {
             return res.status(500).json({errors: error});
@@ -38,5 +41,32 @@ module.exports.registro = async (req, res) =>{
             }
         } catch (error) {
             return res.status(500).json({ errors: error });
+    }
+};
+module.exports.validacaoLogin = [
+    body("email").not().isEmpty().trim().withMessage("Preencha o campo!"),
+    body("senha").not().isEmpty().withMessage("Preencha o campo!")
+];
+module.exports.login = async (req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array})
+    }
+    const {email, senha} = req.body;
+    try{
+        const user = await User.findOne({email})
+        if(user){
+            const equiparar = await bcrypt.compare(senha, user.senha)
+            if(equiparar){
+                const token = createToken(user);
+                return res.status(200).json({msg:'Login feito com sucesso!', token})
+            }else{
+                return res.status(401).json({errors:[{msg: "Senha incorreta!"}]})
+            }
+        }else{
+            return res.status(404).json({errors:[{msg: "email não encontrado!"}]})
+        }
+    }catch (error){
+        return res.status(500).json({errors: error})
     }
 };
--- routes/usuario_rotas.js
+++ routes/usuario_rotas.js
@@ -1,5 +1,9 @@
 const app = require('express')
 const router = app.Router();
-const {registro, validacaoRegistro} = require('../controllers/usuario_controller')
+const {registro, validacaoRegistro, login, validacaoLogin} = require('../controllers/usuario_controller')
+
+
+
 router.post('/registro', validacaoRegistro, registro)
+router.post('/login', login, validacaoLogin)
 module.exports = router;
